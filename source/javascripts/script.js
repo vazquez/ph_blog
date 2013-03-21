839834f1eff30d382328557261fc577d019c3886
Ricardo Vazquez <2ricardo.vazquez@gmail.com>
2013-03-21 16:06:36 -0700

    Adding Google Prettify for code syntax highlighting. Adding Compass support for text-shadows

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,6 +1,11 @@
 //= require "jquery"
 
 $(document).ready(function() {
+  
+  // Make the sidebar as tall as the section height
   console.log( "Section height is: " + $("section").height() + "px");
   $("aside").height(Math.max($("section").height()+300,$("aside").height()));
+  
+  // Find all code tags and add class Prettyprint for code syntax highlighting
+  $("code").addClass("prettyprint");
 });
