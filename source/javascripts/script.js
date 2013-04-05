377ba131e99c548c27f8bdad4f86f95e580f406d
vazquez <ricardo@500px.com>
2013-04-05 16:10:15 -0400

    Adding header. Adding Continure Reading button. Removing <aside>. Restyling hover actions.

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   
   // Make the sidebar as tall as the section height
   console.log( "Section height is: " + $("section").height() + "px");
-  $("aside").height(Math.max($("section").height()+600,$("aside").height()));
+  // $("aside").height(Math.max($("section").height()+600,$("aside").height()));
   
   // Find all code tags and add class Prettyprint for code syntax highlighting
   $("code").addClass("prettyprint");
